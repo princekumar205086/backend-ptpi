7e91b537c03bdf8005cbe474e040b6ef1e7103d8
nidhi334 <knidhibharti8178@gmail.com>
2024-11-26 16:39:25 +0530

    urls fixed

--- ptpi/teacherhire/urls.py
+++ ptpi/teacherhire/urls.py
@@ -5,7 +5,9 @@ from teacherhire.views import (
     SubjectDeleteView, TeacherExperiencesDeleteView, TeacherQualificationDeleteView,
      RegisterUser,
      RegisterUser,
-    LoginUser
+    LoginUser,
+    EducationalQulificationViewSet,TeachersAddressViewSet,TeachersAddressCreateView,EducationalQulificationCreateView
+,TeacherQualificationViewSet,TeacherExperiencesViewSet,SkillViewSet,TeacherSkillViewSet,SkillCreateView,SkillDelete
     )
 from rest_framework import routers
 
@@ -26,4 +28,22 @@ urlpatterns = [
     path('auth/',include('rest_framework.urls',namespace='rest_framework')),
     path('register/', RegisterUser.as_view(), name='register'),
     path('login/', LoginUser.as_view(), name='login'),    
+    path('admin/skill/view/', SkillViewSet.as_view({'get': 'list'}), name='skill'), 
+    path('admin/skill/create/', SkillCreateView.as_view(), name='skill-create'),    
+    path('admin/skill/<int:pk>/', SkillDelete.as_view(), name="skill-delete"),
+    path('teacherSkill/', TeacherSkillViewSet.as_view({'get' : 'list'}), name='teacherskill'), 
+    path('admin/subject/view/', SubjectViewSet.as_view({'get': 'list'}), name='view-subject'),
+    path('admin/subject/create/', SubjectCreateView.as_view(), name='subject-create'),
+    path('admin/subject/<int:pk>/', SubjectDeleteView.as_view(), name='subject-delete'), 
+    path('admin/teacherqualification/create/', TeacherQualificationCreateView.as_view(), name='teacherqualification-create'),
+    path('admin/teacherQualification/<int:pk>/', TeacherQualificationDeleteView.as_view(), name='teacherQualification-delete'),  
+    path('admin/teacherexperiences/create/',TeacherExperiencesCreateView.as_view(), name='teacherexperiences-create'),
+    path('admin/teacherexperiences/<int:pk>/',TeacherExperiencesDeleteView.as_view(), name='teacherexperiences-delete'), 
+    path('login/', LoginUser.as_view(), name='login'), 
+    path('admin/subject/<int:pk>/', SubjectDeleteView.as_view(), name='subject-delete'),  
+
+    path('edQulification/view/', EducationalQulificationViewSet.as_view({'get':'list'}), name='edQulification'),    
+    path('edQulification/create/', EducationalQulificationCreateView.as_view(), name='edQulification-create'),
+    path('teachersAddress/view/', TeachersAddressViewSet.as_view({'get':'list'}), name='teacherAddress'),    
+    path('teachersAddress/create/', TeachersAddressCreateView.as_view(), name='teacherAddress-create'),    
 ]
