f36e1de1afa871a41fa5e79dcffac5404365c745
nidhi334 <knidhibharti8178@gmail.com>
2024-11-26 16:29:40 +0530

    router

--- ptpi/teacherhire/urls.py
+++ ptpi/teacherhire/urls.py
@@ -12,9 +12,22 @@ from teacherhire.views import (
     TeacherQualificationViewSet, TeacherExperiencesViewSet,
     LoginUser
     )
+from rest_framework import routers
+
+
+router = routers.DefaultRouter()
+#router.register(r'subjects',SubjectViewSet)
+router.register(r"admin/teacherexperiences",TeacherExperiencesViewSet)
+router.register(r"admin/teacherqualification",TeacherQualificationViewSet)
+router.register(r"admin/skills",SkillViewSet)
+router.register(r"admin/teacherskills",TeacherSkillViewSet),
+router.register(r"admin/subjects",SubjectViewSet),
+
+
 
 
 urlpatterns = [
+    path('', include(router.urls)),
     path('auth/',include('rest_framework.urls',namespace='rest_framework')),
     path('register/', RegisterUser.as_view(), name='register'),
     path('login/', LoginUser.as_view(), name='login'),    
