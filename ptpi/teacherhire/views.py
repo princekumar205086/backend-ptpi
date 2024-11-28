b84b0ab9a8c0a2300ac506a742f80d7ab0df0663
nidhi334 <knidhibharti8178@gmail.com>
2024-11-28 19:28:09 +0530

    delete work done

--- ptpi/teacherhire/views.py
+++ ptpi/teacherhire/views.py
@@ -45,12 +45,22 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     queryset = UserProfile.objects.all().select_related('user')
     serializer_class = UserProfileSerializer
 
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        instance.delete()
+        return Response({"message": "UserProfile deleted successfully"}, status=status.HTTP_204_NO_CONTENT)
+
 #TeacerAddress GET ,CREATE ,DELETE 
 class TeachersAddressViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated] 
     authentication_classes = [TokenAuthentication]   
     queryset = TeachersAddress.objects.all().select_related('user')
     serializer_class=TeachersAddressSerializer
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        instance.delete()
+        return Response({"message": "TeacherAddress deleted successfully"}, status=status.HTTP_204_NO_CONTENT)
 
 #EducationalQulification GET ,CREATE ,DELETE 
 class EducationalQulificationViewSet(viewsets.ModelViewSet):    
@@ -59,12 +69,22 @@ class EducationalQulificationViewSet(viewsets.ModelViewSet):
     queryset= EducationalQualification.objects.all()
     serializer_class=EducationalQualificationSerializer
 
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        instance.delete()
+        return Response({"message": "EducationalQulification deleted successfully"}, status=status.HTTP_204_NO_CONTENT)
+
 #Subject GET ,CREATE ,DELETE  
 class SkillViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated]
     authentication_classes = [TokenAuthentication] 
     queryset = Skill.objects.all()
     serializer_class = SkillSerializer
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        instance.delete()
+        return Response({"message": "Skill deleted successfully"}, status=status.HTTP_204_NO_CONTENT)
 
 #Teacher GET ,CREATE ,DELETE 
 class TeacherSkillViewSet(viewsets.ModelViewSet):
@@ -73,12 +93,25 @@ class TeacherSkillViewSet(viewsets.ModelViewSet):
     authentication_classes = [TokenAuthentication] 
     serializer_class = TeacherSkillSerializer
 
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        instance.delete()
+        return Response({"message": "TeacherSkill deleted successfully"}, status=status.HTTP_204_NO_CONTENT)
+
 #Subject GET ,CREATE ,DELETE 
 class SubjectViewSet(viewsets.ModelViewSet):    
     permission_classes = [IsAuthenticated] 
     authentication_classes = [TokenAuthentication] 
     queryset= Subject.objects.all()
     serializer_class = SubjectSerializer
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        instance.delete()
+        return Response({"message": "Subject deleted successfully"}, status=status.HTTP_204_NO_CONTENT)
+
+
+
 
 #Teacher GET ,DELETE ,POST
 class TeacherViewSet(viewsets.ModelViewSet):    
@@ -87,12 +120,22 @@ class TeacherViewSet(viewsets.ModelViewSet):
     queryset= Teacher.objects.all()
     serializer_class = TeacherSerializer
 
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        instance.delete()
+        return Response({"message": "Teacher deleted successfully"}, status=status.HTTP_204_NO_CONTENT)
+
 # Classcategory GET ,DELETE ,POST method
 class ClassCategoryViewSet(viewsets.ModelViewSet):    
     permission_classes = [IsAuthenticated]
     authentication_classes = [TokenAuthentication] 
     queryset= ClassCategory.objects.all()
     serializer_class = ClassCategorySerializer
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        instance.delete()
+        return Response({"message": "ClassCategory deleted successfully"}, status=status.HTTP_204_NO_CONTENT)
     
 # TeacherQualification GET ,DELETE ,POST method method
 class TeacherQualificationViewSet(viewsets.ModelViewSet): 
@@ -100,6 +143,11 @@ class TeacherQualificationViewSet(viewsets.ModelViewSet):
     authentication_classes = [TokenAuthentication] 
     queryset = TeacherQualification.objects.all()
     serializer_class = TeacherQualificationSerializer
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        instance.delete()
+        return Response({"message": "TeacherQualification deleted successfully"}, status=status.HTTP_204_NO_CONTENT)
       
 # TeacherExperiences GET ,DELETE ,POST method method
 class TeacherExperiencesViewSet(viewsets.ModelViewSet):
@@ -107,4 +155,9 @@ class TeacherExperiencesViewSet(viewsets.ModelViewSet):
     authentication_classes = [TokenAuthentication] 
     queryset = TeacherExperiences.objects.all()
     serializer_class = TeacherExperiencesSerializer
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        instance.delete()
+        return Response({"message": "TeacherExperiences deleted successfully"}, status=status.HTTP_204_NO_CONTENT)
       
