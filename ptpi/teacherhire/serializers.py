be38c1cc8e414cfe2029d622228faaaf1bac0d9b
nidhi334 <knidhibharti8178@gmail.com>
2024-12-04 15:26:57 +0530

    register bug fixed

--- ptpi/teacherhire/serializers.py
+++ ptpi/teacherhire/serializers.py
@@ -31,6 +31,7 @@ class UserSerializer(serializers.ModelSerializer):
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, required=True)
+    email = serializers.EmailField(required=True)
 
     class Meta:
         model = User
