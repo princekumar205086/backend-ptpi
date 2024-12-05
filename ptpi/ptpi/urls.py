96a17256167a27809336301f79e2bf6be0b196f5
princekumar205086 <prince_kumar_mca21@jimsindia.org>
2024-12-05 16:46:41 +0530

    added superadmin url

--- ptpi/ptpi/urls.py
+++ ptpi/ptpi/urls.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.authtoken import views
 
-
 urlpatterns = [
-    path('api-token-auth/', views.obtain_auth_token),    
-    path("api/",include('teacherhire.urls'))
+    path('api-token-auth/', views.obtain_auth_token),
+    path('api/', include('teacherhire.urls')),
+    path('admin/', admin.site.urls),  # Ensure this line is present only once
 ]
--- ptpi/teacherhire/urls.py
+++ ptpi/teacherhire/urls.py
@@ -4,23 +4,23 @@ from teacherhire.views import *
 from rest_framework import routers
 #from .views import SubjectQuestionsView
 
-#access admin 
+#access admin
 router = routers.DefaultRouter()
-router.register(r"teacher/teacherexperience",TeacherExperiencesViewSet)
-router.register(r"teacher/teacherqualification",TeacherQualificationViewSet)
-router.register(r"admin/skill",SkillViewSet)
-router.register(r"teacher/teacherskill",TeacherSkillViewSet),
-router.register(r"admin/subject",SubjectViewSet),
-router.register(r"admin/classcategory",ClassCategoryViewSet),
-router.register(r"admin/teacher",TeacherViewSet),
-router.register(r'userprofiles', UserProfileViewSet),
-router.register(r'admin/question', QuestionViewSet),
+router.register(r"teacher/teacherexperience", TeacherExperiencesViewSet)
+router.register(r"teacher/teacherqualification", TeacherQualificationViewSet)
+router.register(r"admin/skill", SkillViewSet)
+router.register(r"teacher/teacherskill", TeacherSkillViewSet)
+router.register(r"admin/subject", SubjectViewSet)
+router.register(r"admin/classcategory", ClassCategoryViewSet)
+router.register(r"admin/teacher", TeacherViewSet)
+router.register(r'userprofiles', UserProfileViewSet)
+router.register(r'admin/question', QuestionViewSet)
 router.register(r'admin/educationalQulification', EducationalQulificationViewSet)
 router.register(r'teacher/teachersAddress', TeachersAddressViewSet)
 router.register(r'admin/level', LevelViewSet)
 router.register(r'admin/role', RoleViewSet, basename='role')
 
-#access OnlyTeacher   
+#access OnlyTeacher
 router.register(r'self/teacher', SingleTeacherViewSet, basename='self-teacher')
 router.register(r'self/teacherclasscategory', TeacherClassCategoryViewSet, basename='self-teacherclasscategory')
 router.register(r'self/teachersubject', TeacherSubjectViewSet, basename='self-teachersubject')
@@ -29,10 +29,9 @@ router.register(r'self/teacherAddress', SingleTeachersAddressViewSet, basename='
 router.register(r'self/teacherqualification', SingleTeacherQualificationViewSet, basename='self-teacherqualification')
 router.register(r'self/teacherpreference', PreferenceViewSet, basename='teacher-preference')
 
-
 urlpatterns = [
-    path('',include(router.urls)),
-    path('auth/',include('rest_framework.urls',namespace='rest_framework')),
+    path('', include(router.urls)),
+    path('auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('register/', RegisterUser.as_view(), name='register'),
     path('login/', LoginUser.as_view()),
     path('logout/', LogoutUser.as_view()),
