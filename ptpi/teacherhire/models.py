c9f96aab2b68864c5a52f9fd995d9dbd44cea579
nidhi334 <knidhibharti8178@gmail.com>
2024-12-08 12:58:51 +0530

    some changes

--- ptpi/teacherhire/models.py
+++ ptpi/teacherhire/models.py
@@ -164,6 +164,7 @@ class Question(models.Model):
     time = models.FloatField(default=2.5)
     text = models.CharField(max_length=2000)
     options = models.JSONField()
+    report = models.CharField(max_length=200, default=1, blank=True,null=True)
     correct_option = models.PositiveIntegerField(default=1)
     created_at = models.DateTimeField(auto_now_add=True)
 
