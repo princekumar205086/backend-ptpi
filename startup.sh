24a3e25c82275df40ccb66cb03097067c9193949
princekumar205086 <prince_kumar_mca21@jimsindia.org>
2024-12-15 03:27:45 +0530

    azurel setup

--- startup.sh
+++ startup.sh
@@ -1,80 +1,13 @@
-# Docs for the Azure Web Apps Deploy action: https://github.com/Azure/webapps-deploy
-# More GitHub Actions for Azure: https://github.com/Azure/actions
-# More info on Python, GitHub Actions, and Azure App Service: https://aka.ms/python-webapps-actions
+#!/bin/bash
 
-name: Build and deploy Python app to Azure Web App - ptpi
+# Install dependencies
+pip install -r requirements.txt
 
-on:
-  push:
-    branches:
-      - main
-  workflow_dispatch:
+# Run migrations
+python manage.py migrate
 
-jobs:
-  build:
-    runs-on: ubuntu-latest
+# Collect static files
+python manage.py collectstatic --noinput
 
-    steps:
-      - uses: actions/checkout@v4
-
-      - name: Set up Python version
-        uses: actions/setup-python@v5
-        with:
-          python-version: '3.12'
-
-      - name: Create and start virtual environment
-        run: |
-          python -m venv venv
-          source venv/bin/activate
-      
-      - name: Install dependencies
-        run: pip install -r requirements.txt
-        
-      # Optional: Add step to run tests here (PyTest, Django test suites, etc.)
-
-      - name: Zip artifact for deployment
-        run: zip release.zip ./* -r
-
-      - name: Upload artifact for deployment jobs
-        uses: actions/upload-artifact@v4
-        with:
-          name: python-app
-          path: |
-            release.zip
-            !venv/
-
-  deploy:
-    runs-on: ubuntu-latest
-    needs: build
-    environment:
-      name: 'Production'
-      url: ${{ steps.deploy-to-webapp.outputs.webapp-url }}
-    permissions:
-      id-token: write #This is required for requesting the JWT
-
-    steps:
-      - name: Download artifact from build job
-        uses: actions/download-artifact@v4
-        with:
-          name: python-app
-
-      - name: Unzip artifact for deployment
-        run: unzip release.zip
-
-      - name: Make startup script executable
-        run: chmod +x startup.sh
-
-      - name: Login to Azure
-        uses: azure/login@v2
-        with:
-          client-id: ${{ secrets.AZUREAPPSERVICE_CLIENTID_636600CDB624446495860AB091A6C2EE }}
-          tenant-id: ${{ secrets.AZUREAPPSERVICE_TENANTID_685D43E605384B2D8B1E4900D401A760 }}
-          subscription-id: ${{ secrets.AZUREAPPSERVICE_SUBSCRIPTIONID_98B3279CC9B044B7A8770B0AED72C70C }}
-
-      - name: 'Deploy to Azure Web App'
-        uses: azure/webapps-deploy@v3
-        id: deploy-to-webapp
-        with:
-          app-name: 'ptpi'
-          slot-name: 'Production'
-          startup-command: './startup.sh'
+# Start the Gunicorn server
+gunicorn --bind 0.0.0.0:8000 ptpi.wsgi:application
